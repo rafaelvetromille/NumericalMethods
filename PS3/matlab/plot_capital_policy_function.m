558676ac2c8a94abfa97bd36419ee4ba9da64380
Rafael Vetromille <53228280+rafaelvetromille@users.noreply.github.com>
2022-05-27 17:21:49 -0300

    lista 3

---
+++ PS3/matlab/plot_capital_policy_function.m
@@ -0,0 +1,39 @@
+function plot_capital_policy_function(variable, kgrid, zgrid)
+
+% Necessário para plotar o gráfico em 3D
+k            = repelem(kgrid,1,7);
+z            = repelem(zgrid,1,length(kgrid))';
+nz           = length(zgrid);
+
+% Gráfico (3D) - Função política: k' = g(k,z)
+subplot(1,2,1);
+set(gcf,'DefaultLineLineWidth', 1.5);
+surf(k, z, variable, 'FaceAlpha', 0.8, 'EdgeColor', 'none')
+set(get(gcf,'CurrentAxes'),'FontSize',14,'LineWidth', 1.5)
+set(gca,'DefaultTextInterpreter', 'latex','TickLabelInterpreter','latex') 
+xlabel('estoque de capital, $k$','Interpreter','latex');
+ylabel('realiza\c{c}{\~o}es de produtividade, $z$','Interpreter','latex');
+zlabel('fun\c{c}{\~a}o  pol{\''i}tica, $k^\prime = g(k,z)$','Interpreter','latex');
+title('\textbf{Fun\c{c}{\~a}o pol{\''i}tica $$k^\prime = g(k,z)$$}','interpreter','latex','fontsize',20);
+axis([min(kgrid) max(kgrid) min(zgrid) max(zgrid) min(min(variable)) max(max(variable))]) 
+box on 
+
+% Gráfico (2D) - Função política: k' = g(k,z)
+subplot(1,2,2);
+set(gcf,'DefaultLineLineWidth', 1.5);
+hold on
+for i = 1:nz
+    plot(kgrid, variable(:,i), 'DisplayName', sprintf('$z_{%4i} = %6.2f$', [i, zgrid(i)]));   
+end
+legend('show', 'Location', 'southeast')
+set(get(gcf,'CurrentAxes'),'FontSize',14,'LineWidth', 1.5)
+set(gca,'DefaultTextInterpreter', 'latex','TickLabelInterpreter','latex') 
+set(get(gcf,'CurrentAxes'),'FontSize',14,'LineWidth', 1.5)
+set(gca,'DefaultTextInterpreter', 'latex','TickLabelInterpreter','latex') 
+xlabel('estoque de capital, $k$','Interpreter','latex');
+ylabel('fun\c{c}{\~a}o pol{\''i}tica, $k^\prime = g(k,z)$','Interpreter','latex');
+title('\textbf{Fun\c{c}{\~a}o pol{\''i}tica $k^\prime = g(k,z)$ para v{\''a}rios $$z$$''s}','interpreter','latex','fontsize',20);
+axis([min(kgrid) max(kgrid) min(min(variable)) max(max(variable))]) 
+box on
+
+end
--- PS3/matlab/teste.m
+++
@@ -1,6 +0,0 @@
-syms x y
-fplot(chebyshevT(0:4,x))
-axis([-1 1 -1 1])
-grid on
-
-title('Chebyshev polynomials of the first kind')
