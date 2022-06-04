eb5443217fc2ad58d6d88be465ca1812cbc39dc3
Rafael Vetromille <53228280+rafaelvetromille@users.noreply.github.com>
2022-06-03 23:15:28 -0300

    all

--- PS3/matlab/psi_i.m
+++ PS3/matlab/psi_i.m
@@ -1,63 +1,38 @@
-function valor = psi_i(i,k, n_k)
-
-%%%%%%%%%% DEFININDO PARÂMETROS %%%%%%%
+function psi = psi_i(i, k, nk)
 
 alpha = 1/3;
-beta = 0.987;
+beta  = 0.987;
 delta = 0.012;
 
-%%%%%%% TAMANHO DO GRID DO CAPITAL
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%%%%%%% CRIAR O GRID DO CAPITAL %%%%%%%%%%%%
-
-k_ss = (1/beta + delta - 1)*(1/alpha);
-k_ss = k_ss^(1/(alpha - 1));
-
-grid_k = linspace(0.75*k_ss, 1.25*k_ss, n_k);
+k_ss  = ((1/beta + delta - 1)*(1/alpha))^(1/(alpha - 1));
+kgrid = linspace(0.75*k_ss, 1.25*k_ss, nk);
 
 if i == 1
     
-    if k >= grid_k(i) & k <= grid_k(i+1)
-    
-    valor = (grid_k(i+1) - k)/(grid_k(i+1) - grid_k(i));
-    
-    else
-        
-        valor = 0;
-        
+    if k >= kgrid(i) && k <= kgrid(i+1)   
+        psi = (kgrid(i+1) - k)/(kgrid(i+1) - kgrid(i));   
+    else 
+        psi = 0;
     end
     
-elseif i == n_k
+elseif i == nk
     
-    if k <= grid_k(i) & k >= grid_k(i-1)
-    
-    valor = (k - grid_k(i-1))/(grid_k(i) - grid_k(i-1));
-    
-    else
-        
-        valor = 0;
-        
+    if k <= kgrid(i) && k >= kgrid(i-1)     
+        psi = (k - kgrid(i-1))/(kgrid(i) - kgrid(i-1));
+    else    
+        psi = 0;
     end
     
 else
-        
-       if k >= grid_k(i-1) & k <= grid_k(i) 
-           
-            valor = (k - grid_k(i-1))/(grid_k(i) - grid_k(i-1));
-            
-       elseif k > grid_k(i) & k <= grid_k(i+1)
-           valor = (grid_k(i+1) - k)/(grid_k(i+1) - grid_k(i));
-           
-       else 
-           valor = 0;
-           
-       end
+    
+    if k >= kgrid(i-1) && k <= kgrid(i)
+        psi = (k - kgrid(i-1))/(kgrid(i) - kgrid(i-1));  
+    elseif k >= kgrid(i) && k <= kgrid(i+1)
+        psi = (kgrid(i+1) - k)/(kgrid(i+1) - kgrid(i)); 
+    else
+        psi = 0;
+    end
 end
-
-
 
 end
 
